9eae171c353efb1cd81c39f507230a9cbd079364
Shukria.I <iqbal_shukria@hotmail.com>
2023-02-22 22:48:01 -0500

    this is my test

---
+++ src/testOne/ClearOne.java
@@ -0,0 +1,13 @@
+package testOne;
+
+public class ClearOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a = 23, b = 34 , k =45;
+    
+    System.out.println(a+b+k);
+    
+	}
+
+}
